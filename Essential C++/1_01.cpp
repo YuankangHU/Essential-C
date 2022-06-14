357856ab183fafb87f8409c0984734423589850c
YuankangHU <hubert.yuankang@gmail.com>
2022-06-15 01:32:28 +0200

    Yuankang
    first day

---
+++ Essential C++/1_01.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+    string user_name;
+
+    cout << "Please enter your name: ";
+    cin >> user_name;
+    cout << "Hello\n" << "Hello " << user_name << " and goodbye ";
+
+    return 0;
+}
