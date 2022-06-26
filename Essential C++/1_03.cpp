9356b581dc7a846ffc5ab7fa184d07b5c3b0f51e
YuankangHU <hubert.yuankang@gmail.com>
2022-06-26 17:26:28 +0200

    更新

--- Essential C++/1_03.cpp
+++ Essential C++/1_03.cpp
@@ -8,13 +8,15 @@ int main(void)
     vector<int>pell_seq(seq_size);
     pell_seq[0] = 1;
     pell_seq[1] = 2;
+    
     for(int index = 2; index<seq_size; index++)
-    {
         pell_seq[index] = 2*pell_seq[index-1] + pell_seq[index-2];
-    }
+    
     cout << "The first " << pell_seq.size() << " elementss of the Pell series:"<<endl;
+    
     for(int index = 0; index<seq_size; index++)
         cout << pell_seq[index] << " ";
+    
     cout << endl;
     
     return 0;
